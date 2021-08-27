dd4bd88528fe9dab10a9969fd90c358004f26e34
gooriiie <fkstldghks123@naver.com>
2021-08-27 14:41:56 +0900

    Controller

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -26,13 +26,13 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status " +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) // 최대 1000건
-                .getResultList();
+        return em.createQuery("select o from Order o join o.member m" +
+                    " where o.status = :status " +
+                    " and m.name like :name", Order.class)
+                    .setParameter("status", orderSearch.getOrderStatus())
+                    .setParameter("name", orderSearch.getMemberName())
+                    .setMaxResults(1000) // 최대 1000건
+                    .getResultList();
     }
 
     /**
@@ -65,6 +65,6 @@ public class OrderRepository {
         return query.getResultList();
     }
 
-    
+
 
 }
